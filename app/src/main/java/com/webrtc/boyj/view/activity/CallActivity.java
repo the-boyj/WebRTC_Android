30cfd7829156f91cdd3b557414d9a010f22c1930
Yoon Young-jik <tbtzpdlql@gmail.com>
2019-01-19 17:56:49 +0900

    Initial call activity

---
+++ app/src/main/java/com/webrtc/boyj/view/activity/CallActivity.java
@@ -0,0 +1,15 @@
+package com.webrtc.boyj.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webrtc.boyj.R;
+
+public class CallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+}

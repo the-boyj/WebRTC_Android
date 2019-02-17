7ba793a721b957ba02138a2b7e607350ebff2885
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-18 00:32:38 +0900

    #028 레이아웃 색상 최적화

--- app/src/main/java/com/webrtc/boyj/presentation/main/MainActivity.java
+++ app/src/main/java/com/webrtc/boyj/presentation/main/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -26,7 +27,16 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initToolbar();
         checkPermission();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(binding.toolbar);
+        ActionBar toolbar = getSupportActionBar();
+        if (toolbar != null) {
+            toolbar.setDisplayShowTitleEnabled(false);
+        }
     }
 
     @SuppressLint({"MissingPermission", "HardwareIds"})

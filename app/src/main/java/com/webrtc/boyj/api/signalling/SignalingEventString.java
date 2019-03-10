7489998a9228108c41da1b7a9c9f5a824ea226fc
bhw <bhw1994@gmail.com>
2019-03-10 21:56:27 +0900

    시그널링 이벤트 스트링 클래스 추가 기존 인터페이스 삭제

---
+++ app/src/main/java/com/webrtc/boyj/api/signalling/SignalingEventString.java
@@ -0,0 +1,19 @@
+package com.webrtc.boyj.api.signalling;
+
+public class SignalingEventString {
+    public static final String EVENT_DIAL ="dial";
+    public static final String EVENT_CREATED = "created";
+    public static final String EVENT_AWAKEN = "awaken";
+    public static final String EVENT_KNOCK = "knock";
+    public static final String EVENT_ACCEPT = "accept";
+    public static final String EVENT_REJECT = "reject";
+    public static final String EVENT_READY = "ready";
+    public static final String EVENT_SEND_SDP = "ssdp";
+    public static final String EVENT_RECEIVE_SDP = "rsdp";
+    public static final String EVENT_SEND_ICE = "sice";
+    public static final String EVENT_RECEIVE_ICE = "rice";
+    public static final String EVENT_BYE = "bye";
+    public static final String EVENT_LOG = "log";
+    public static final String EVENT_SERVER_ERROR = "serverError";
+    public static final String EVENT_PEER_ERROR = "peerError";
+}

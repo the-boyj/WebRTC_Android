2f1d86cf67542db7b34dd301ef849a241e63b1f2
bhw1994 <bhw1994@gmail.com>
2019-03-18 14:25:48 +0900

    IceCandidatePayload 추가 SdpPayload 추가

---
+++ app/src/main/java/com/webrtc/boyj/api/signalling/payload/IceCandidatePayload.java
@@ -0,0 +1,36 @@
+package com.webrtc.boyj.api.signalling.payload;
+
+import android.support.annotation.NonNull;
+
+import org.webrtc.IceCandidate;
+
+public class IceCandidatePayload {
+
+    @NonNull
+    private IceCandidate iceCandidate;
+
+    public void setIceCandidate(@NonNull final IceCandidate iceCandidate) {
+        this.iceCandidate = iceCandidate;
+    }
+
+    public static class Builder {
+        @NonNull
+        private final IceCandidate iceCandidate;
+
+        public Builder(@NonNull final IceCandidate iceCandidate) {
+            this.iceCandidate = iceCandidate;
+        }
+
+        @NonNull
+        public IceCandidatePayload build() {
+            final IceCandidatePayload iceCandidatePayload = new IceCandidatePayload();
+            iceCandidatePayload.setIceCandidate(this.iceCandidate);
+            return iceCandidatePayload;
+        }
+    }
+
+
+}
+
+
+

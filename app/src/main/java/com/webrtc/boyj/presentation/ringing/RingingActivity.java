e73e650095df6ff68fd2e3897e9a767cdae7f034
bhw1994 <bhw1994@gmail.com>
2019-04-03 14:29:38 +0900

    ringing activity 개선

--- app/src/main/java/com/webrtc/boyj/presentation/ringing/RingingActivity.java
+++ app/src/main/java/com/webrtc/boyj/presentation/ringing/RingingActivity.java
@@ -1,5 +1,6 @@
 package com.webrtc.boyj.presentation.ringing;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,45 +9,49 @@ import android.support.annotation.Nullable;
 
 import com.webrtc.boyj.R;
 import com.webrtc.boyj.api.signalling.payload.FCMPayload;
-import com.webrtc.boyj.data.model.User;
 import com.webrtc.boyj.databinding.ActivityRingingBinding;
 import com.webrtc.boyj.presentation.BaseActivity;
 import com.webrtc.boyj.presentation.call.CallActivity;
 
 public class RingingActivity extends BaseActivity<ActivityRingingBinding> {
-
     private static final String EXTRA_FCM_PAYLOAD = "EXTRA_FCM_PAYLOAD";
+    private FCMPayload payload;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Todo : get User information as FCM
+        payload = (FCMPayload) getIntent().getSerializableExtra(EXTRA_FCM_PAYLOAD);
+
         initViews();
+        initViewModel();
     }
 
     private void initViews() {
         findViewById(R.id.fab_accept).setOnClickListener(__ -> startCallActivity());
-        findViewById(R.id.fab_reject).setOnClickListener(__ -> finish());
+        findViewById(R.id.fab_reject).setOnClickListener(__ -> {
+            binding.getVm().reject();
+            finish();
+        });
+    }
+
+    private void initViewModel() {
+        final RingingViewModel vm = ViewModelProviders.of(this,
+                new RingingViewModelFactory(payload.getTel())).get(RingingViewModel.class);
+        binding.setVm(vm);
     }
 
     private void startCallActivity() {
-        // Mock data
-        final User otherUser = new User("010-2222-3333", "홍길동", "10129129");
-
-        startActivity(CallActivity.getLaunchIntent(this,
-                otherUser, null, false));
+        startActivity(CallActivity.getLaunchIntent(this, payload.getTel(), payload.getRoom(), false));
         overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
         finish();
     }
-
 
     public static Intent getLaunchIntent(@NonNull final Context context,
                                          @NonNull final FCMPayload payload) {
         return getLaunchIntent(context, RingingActivity.class)
                 .putExtra(EXTRA_FCM_PAYLOAD, payload);
     }
-
 
     @Override
     protected int getLayoutId() {
--- app/src/main/java/com/webrtc/boyj/presentation/ringing/RingingViewModel.java
+++ app/src/main/java/com/webrtc/boyj/presentation/ringing/RingingViewModel.java
@@ -3,22 +3,25 @@ package com.webrtc.boyj.presentation.ringing;
 import android.support.annotation.NonNull;
 
 import com.webrtc.boyj.api.BoyjRTC;
-import com.webrtc.boyj.api.signalling.SignalingClient;
+import com.webrtc.boyj.presentation.BaseViewModel;
 
-public class RingingViewModel {
-
+public class RingingViewModel extends BaseViewModel {
+    @NonNull
+    private final String tel;
     @NonNull
     private final BoyjRTC boyjRTC;
 
-    public RingingViewModel() {
-        boyjRTC = new BoyjRTC();
+    RingingViewModel(@NonNull String tel) {
+        this.tel = tel;
+        this.boyjRTC = new BoyjRTC();
     }
 
-    public void accept() {
-        boyjRTC.accept();
-    }
-
-    public void reject() {
+    void reject() {
         boyjRTC.reject();
+    }
+
+    @NonNull
+    public String getTel() {
+        return tel;
     }
 }

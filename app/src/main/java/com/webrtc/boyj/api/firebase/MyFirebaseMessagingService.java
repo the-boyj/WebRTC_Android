1c106d4cb7843a8286a563df795e738b321a968d
bhw1994 <bhw1994@gmail.com>
2019-04-03 14:43:57 +0900

    FirebaseMessagingService 수정

--- app/src/main/java/com/webrtc/boyj/api/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/webrtc/boyj/api/firebase/MyFirebaseMessagingService.java
@@ -12,24 +12,20 @@ import com.webrtc.boyj.api.signalling.payload.AwakenPayload;
 import com.webrtc.boyj.api.signalling.payload.FCMPayload;
 import com.webrtc.boyj.data.repository.UserRepositoryImpl;
 import com.webrtc.boyj.presentation.ringing.RingingActivity;
-import com.webrtc.boyj.utils.App;
 import com.webrtc.boyj.utils.Logger;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
-
-    @NonNull
-    private static final String TAG = "BOYJ_MyFCMService";
 
     @Override
     public void onMessageReceived(@NonNull final RemoteMessage remoteMessage) {
         Logger.d("FCM received");
 
         final FCMPayload payload = new FCMPayload(remoteMessage);
-
         final BoyjRTC boyjRTC = new BoyjRTC();
+
         boyjRTC.knock().subscribe(() -> {
             final Intent intent = RingingActivity.getLaunchIntent(getApplicationContext(), payload);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         });
 
@@ -37,7 +33,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         final AwakenPayload awakenPayload = new AwakenPayload.Builder(room).build();
         boyjRTC.awaken(awakenPayload);
     }
-
 
     @Override
     public void onNewToken(@NonNull final String token) {
@@ -47,5 +42,4 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .putBoolean(UserRepositoryImpl.CHANGED, true)
                 .apply();
     }
-
 }

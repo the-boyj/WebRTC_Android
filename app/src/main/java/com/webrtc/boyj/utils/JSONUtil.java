69e712c31933a9a71ccfff6e4c64b769c9cc9e26
tbtzpdlql <tbtzpdlql@gmail.com>
2019-04-07 23:08:06 +0900

    #089 Add "JSONUtil" for use "toJsonObject" method

---
+++ app/src/main/java/com/webrtc/boyj/utils/JSONUtil.java
@@ -0,0 +1,29 @@
+package com.webrtc.boyj.utils;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONUtil {
+    @NonNull
+    private static final Gson gson = new Gson();
+
+    /**
+     * Object를 JsonObject로 변환한다.
+     *
+     * @throws IllegalStateException 오브젝트를 JSONObject으로 변환할 수 없는 경우(JSONException) 발생
+     */
+    @NonNull
+    public static JSONObject toJSONObject(@NonNull final Object object) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(gson.toJson(object));
+        } catch (JSONException e) {
+            throw new IllegalStateException("object is not valid");
+        }
+        return jsonObject;
+    }
+}

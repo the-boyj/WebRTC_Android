5415d95348427d646c72706ef64484f89add96f9
tbtzpdlql <tbtzpdlql@gmail.com>
2019-04-07 23:00:47 +0900

    #089 Add "CreateRoomPayload"

---
+++ app/src/main/java/com/webrtc/boyj/api/signalling/payload/CreateRoomPayload.java
@@ -0,0 +1,54 @@
+package com.webrtc.boyj.api.signalling.payload;
+
+import android.support.annotation.NonNull;
+
+import java.util.UUID;
+
+public class CreateRoomPayload {
+    @NonNull
+    private final String room; // UUID로 생성되는 방 정보
+    @NonNull
+    private final String callerId; // 통화 송신자의 식별 번호
+
+    private CreateRoomPayload(@NonNull final String room,
+                              @NonNull final String callerId) {
+        this.room = room;
+        this.callerId = callerId;
+    }
+
+    @NonNull
+    public String getRoom() {
+        return room;
+    }
+
+    @NonNull
+    public String getCallerId() {
+        return callerId;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("'CreateRoomPayload' : {'room' : {%s}, 'callerId' : {%s}}", room, callerId);
+    }
+
+    public static class Builder {
+        @NonNull
+        private final String room;
+        @NonNull
+        private final String callerId;
+
+        public Builder(@NonNull final String callerId) {
+            this.room = createRoom(callerId);
+            this.callerId = callerId;
+        }
+
+        public CreateRoomPayload build() {
+            return new CreateRoomPayload(room, callerId);
+        }
+
+        private static String createRoom(@NonNull final String callerId) {
+            return UUID.fromString(callerId).toString();
+        }
+    }
+}

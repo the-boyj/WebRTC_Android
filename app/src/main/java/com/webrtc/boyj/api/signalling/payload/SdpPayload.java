4f75af825b2eb6c76301ebbad4c5f9ca088cedfd
tbtzpdlql <tbtzpdlql@gmail.com>
2019-04-14 19:36:20 +0900

    #105 Integrate "AcceptPayload" to "SdpPayload"

--- app/src/main/java/com/webrtc/boyj/api/signalling/payload/SdpPayload.java
+++ app/src/main/java/com/webrtc/boyj/api/signalling/payload/SdpPayload.java
@@ -9,6 +9,15 @@ import org.webrtc.SessionDescription;
 public class SdpPayload {
     private SessionDescription sdp;
     private String sender;
+    private String receiver;
+
+    public SdpPayload() {
+
+    }
+
+    public SdpPayload(@NonNull final SessionDescription sdp) {
+        this.sdp = sdp;
+    }
 
     public SessionDescription getSdp() {
         return sdp;
@@ -24,6 +33,18 @@ public class SdpPayload {
 
     public void setSender(String sender) {
         this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public SessionDescription.Type getType() {
+        return sdp.type;
     }
 
     @NonNull

5b75aad1618e691fbcf1716b7df6c8376ad47b4a
tbtzpdlql <tbtzpdlql@gmail.com>
2019-04-16 14:22:22 +0900

    #113 Update IceCandidatePayload

--- app/src/main/java/com/webrtc/boyj/api/signalling/payload/IceCandidatePayload.java
+++ app/src/main/java/com/webrtc/boyj/api/signalling/payload/IceCandidatePayload.java
@@ -2,49 +2,50 @@ package com.webrtc.boyj.api.signalling.payload;
 
 import android.support.annotation.NonNull;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.webrtc.boyj.utils.JSONUtil;
 
-import org.json.JSONObject;
 import org.webrtc.IceCandidate;
 
-public class IceCandidatePayload extends Payload {
-
-    @NonNull
+public class IceCandidatePayload {
     private IceCandidate iceCandidate;
+    private String sender;
+    private String receiver;
 
-    public void setIceCandidate(@NonNull final IceCandidate iceCandidate) {
+    public IceCandidatePayload() {
+
+    }
+
+    public IceCandidatePayload(@NonNull final IceCandidate iceCandidate) {
         this.iceCandidate = iceCandidate;
     }
 
-    @NonNull
-    public static IceCandidatePayload fromJsonObject(@NonNull final JSONObject jsonObject) {
-        return new Gson().fromJson(jsonObject.toString(), IceCandidatePayload.class);
-    }
-
-    @NonNull
     public IceCandidate getIceCandidate() {
         return iceCandidate;
     }
 
-    public static class Builder {
-        @NonNull
-        private final IceCandidate iceCandidate;
-
-        public Builder(@NonNull final IceCandidate iceCandidate) {
-            this.iceCandidate = iceCandidate;
-        }
-
-        @NonNull
-        public IceCandidatePayload build() {
-            final IceCandidatePayload iceCandidatePayload = new IceCandidatePayload();
-            iceCandidatePayload.setIceCandidate(this.iceCandidate);
-            return iceCandidatePayload;
-        }
+    public void setIceCandidate(IceCandidate iceCandidate) {
+        this.iceCandidate = iceCandidate;
     }
 
+    public String getSender() {
+        return sender;
+    }
 
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return JSONUtil.toJson(getClass(), this);
+    }
 }
-
-
-

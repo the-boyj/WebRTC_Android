9518db6a08dff9696f8f4385a582aaa6a8dca996
bhw1994 <bhw1994@gmail.com>
2019-05-05 15:33:15 +0900

    Rtc Config 설정 기능추가

---
+++ app/src/main/java/com/webrtc/boyj/presentation/settings/SettingsFragment.java
@@ -0,0 +1,45 @@
+package com.webrtc.boyj.presentation.settings;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.webrtc.boyj.R;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        getPreferenceManager().setSharedPreferencesName("RtcConfiguration");
+        setPreferencesFromResource(R.xml.rtc_config,rootKey);
+
+    }
+
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(sharedPreferences.getString(key, ""));
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(sharedPreferences.getString(key, ""));
+
+        }
+    }
+}
+
+

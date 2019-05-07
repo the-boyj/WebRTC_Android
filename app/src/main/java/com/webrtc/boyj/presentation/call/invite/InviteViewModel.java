9b21930d3fb4b250f48a6f68e21d685a78e6b589
tbtzpdlql <tbtzpdlql@gmail.com>
2019-05-07 19:32:46 +0900

    #169 초대 기능 불필요한 버튼 삭제 및 최적화

--- app/src/main/java/com/webrtc/boyj/presentation/call/invite/InviteViewModel.java
+++ app/src/main/java/com/webrtc/boyj/presentation/call/invite/InviteViewModel.java
@@ -7,31 +7,33 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.webrtc.boyj.data.model.User;
-import com.webrtc.boyj.data.source.UserRepository;
+import com.webrtc.boyj.data.source.local.room.dao.UserDao;
+import com.webrtc.boyj.data.source.local.room.entity.UserMapper;
 import com.webrtc.boyj.presentation.BaseViewModel;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class InviteViewModel extends BaseViewModel {
     @NonNull
-    private final UserRepository repository;
+    private final UserDao userDao;
     @NonNull
     private final MutableLiveData<List<User>> otherUserList = new MutableLiveData<>();
 
-    private InviteViewModel(@NonNull final UserRepository repository) {
-        this.repository = repository;
+    public InviteViewModel(@NonNull UserDao userDao) {
+        this.userDao = userDao;
     }
 
     public void loadOtherUserList(@NonNull final List<String> ids) {
-        addDisposable(repository.getOtherUserListExceptIds(ids)
+        addDisposable(userDao.selectExceptIds(ids)
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(users -> {
-                    final List<User> userList = new ArrayList<>(users);
-                    this.otherUserList.setValue(userList);
-                }, Throwable::printStackTrace)
+                .map(UserMapper::toUserListFromEntities)
+                .onErrorReturnItem(Collections.emptyList())
+                .subscribe(this.otherUserList::setValue, Throwable::printStackTrace)
         );
     }
 
@@ -40,10 +42,10 @@ public class InviteViewModel extends BaseViewModel {
     }
 
     public static class Factory implements ViewModelProvider.Factory {
-        private final UserRepository repository;
+        private final UserDao userDao;
 
-        public Factory(@NonNull final UserRepository repository) {
-            this.repository = repository;
+        public Factory(UserDao userDao) {
+            this.userDao = userDao;
         }
 
         @NonNull
@@ -51,7 +53,7 @@ public class InviteViewModel extends BaseViewModel {
         @SuppressWarnings("unchecked")
         public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
             if (modelClass.isAssignableFrom(InviteViewModel.class)) {
-                return (T) new InviteViewModel(repository);
+                return (T) new InviteViewModel(userDao);
             } else {
                 throw new IllegalArgumentException("ViewModel Not Found");
             }

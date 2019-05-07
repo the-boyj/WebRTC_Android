d1951fc79435c61655aba2e2c30345a04548b515
tbtzpdlql <tbtzpdlql@gmail.com>
2019-05-08 00:00:47 +0900

    #173 Local SurfaceView 드래그 기능 추가 & 크기 확대

--- app/src/main/java/com/webrtc/boyj/presentation/common/view/BoyjSurfaceView.java
+++ app/src/main/java/com/webrtc/boyj/presentation/common/view/BoyjSurfaceView.java
@@ -1,7 +1,10 @@
 package com.webrtc.boyj.presentation.common.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.webrtc.boyj.api.boyjrtc.peer.manager.EglBaseManager;
 
@@ -9,6 +12,8 @@ import org.webrtc.RendererCommon;
 import org.webrtc.SurfaceViewRenderer;
 
 public class BoyjSurfaceView extends SurfaceViewRenderer {
+    private float dx, dy;
+
     public BoyjSurfaceView(Context context) {
         super(context);
         initView();
@@ -25,4 +30,35 @@ public class BoyjSurfaceView extends SurfaceViewRenderer {
         setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
         setEnableHardwareScaler(true);
     }
+
+    public void dragStart() {
+        setOnTouchListener(onTouchListener);
+    }
+
+    private final OnTouchListener onTouchListener = new OnTouchListener() {
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+
+                case MotionEvent.ACTION_DOWN:
+
+                    dx = v.getX() - event.getRawX();
+                    dy = v.getY() - event.getRawY();
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+
+                    v.animate()
+                            .x(event.getRawX() + dx)
+                            .y(event.getRawY() + dy)
+                            .setDuration(0)
+                            .start();
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+    };
 }

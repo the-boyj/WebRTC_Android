ca00b5943f614d10a152adcb82880f135522276f
tbtzpdlql <tbtzpdlql@gmail.com>
2019-05-07 19:40:07 +0900

    #169 FAB 우측 버튼 초대 기능으로 변경

--- app/src/main/java/com/webrtc/boyj/presentation/call/CallActivity.java
+++ app/src/main/java/com/webrtc/boyj/presentation/call/CallActivity.java
@@ -3,7 +3,6 @@ package com.webrtc.boyj.presentation.call;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -42,6 +41,7 @@ public class CallActivity extends BaseActivity<ActivityCallBinding> {
 
     private void initViews() {
         initSplitLayout();
+        initInviteFab();
     }
 
     private void initSplitLayout() {
@@ -50,15 +50,17 @@ public class CallActivity extends BaseActivity<ActivityCallBinding> {
         splitLayout.setAdapter(adapter);
     }
 
-    public void showCallMenuDialog(View view) {
-        final List<String> ids = adapter.getUserListInRoomIncludingMe(id);
-        final CallMenuDialog dialog = CallMenuDialog.newInstance(ids);
-        dialog.setOnInviteListener(user -> {
-            showToast(user.getName() + " 에게 통화를 요청하였습니다.");
-            callViewModel.invite(user.getId());
-            dialog.dismiss();
+    private void initInviteFab() {
+        findViewById(R.id.fab_right).setOnClickListener(__ -> {
+            final List<String> ids = adapter.getUserListInRoomIncludingMe(id);
+            final CallMenuDialog dialog = CallMenuDialog.newInstance(ids);
+            dialog.setOnInviteListener(user -> {
+                showToast(user.getName() + " 에게 통화를 요청하였습니다.");
+                callViewModel.invite(user.getId());
+                dialog.dismiss();
+            });
+            dialog.show(getSupportFragmentManager(), "CallMenuDialog");
         });
-        dialog.show(getSupportFragmentManager(), "CallMenuDialog");
     }
 
     private void initViewModel() {

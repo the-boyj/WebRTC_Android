057a7c0097c4d38cbf31e28adf4c1ab20ea9ab6e
Laco <latinacoding@gmail.com>
2019-05-28 17:48:24 +0900

    #182 IceServerList 업데이트

--- app/src/main/java/com/webrtc/boyj/api/boyjrtc/peer/IceServers.java
+++ app/src/main/java/com/webrtc/boyj/api/boyjrtc/peer/IceServers.java
@@ -7,38 +7,29 @@ import java.util.Arrays;
 import java.util.List;
 
 public class IceServers {
+
+    private static final String CREDENTIAL_ID = "boyj";
+    private static final String CREDENTIAL_PWD = "boyj";
+
+    private static final List<String> serverUrlList = new ArrayList<>(
+            Arrays.asList(
+                    "turn:13.124.30.187:3478",
+                    "turn:13.209.192.5:3478"
+            )
+    );
+
     private IceServers() {
 
     }
 
     public static List<PeerConnection.IceServer> getIceServerList() {
         final List<PeerConnection.IceServer> iceServerList = new ArrayList<>();
-        final List<String> stunUrlList = new ArrayList<>(
-                Arrays.asList("stun:tk-turn1.xirsys.com")
-        );
-        final List<String> turnUrlList = new ArrayList<>(
-                Arrays.asList(
-                        "turn:tk-turn1.xirsys.com:80?transport=udp",
-                        "turn:tk-turn1.xirsys.com:3478?transport=udp",
-                        "turn:tk-turn1.xirsys.com:80?transport=tcp",
-                        "turn:tk-turn1.xirsys.com:3478?transport=tcp",
-                        "turns:tk-turn1.xirsys.com:443?transport=tcp",
-                        "turns:tk-turn1.xirsys.com:5349?transport=tcp"
-                )
-        );
 
-        for (final String stunServerUrl : stunUrlList) {
+        for (final String url : serverUrlList) {
             iceServerList.add(
-                    PeerConnection.IceServer.builder(stunServerUrl)
-                            .createIceServer()
-            );
-        }
-
-        for (final String turnServerUrl : turnUrlList) {
-            iceServerList.add(
-                    PeerConnection.IceServer.builder(turnServerUrl)
-                            .setUsername("aa1f1c54-39c4-11e9-9ab4-8a1138a37ce0")
-                            .setPassword("aa1f1ccc-39c4-11e9-9fd9-42348e526b10")
+                    PeerConnection.IceServer.builder(url)
+                            .setUsername(CREDENTIAL_ID)
+                            .setPassword(CREDENTIAL_PWD)
                             .createIceServer()
             );
         }
